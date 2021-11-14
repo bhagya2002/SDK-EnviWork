779ec461ac7ccf2a7b0b450393dd9d5628ddb7c7
Kevin_was_here <xuyang4@ualberta.ca>
2021-11-14 11:57:46 -0700

    "most updated changes - quote gen still not working"

--- website/static/index.js
+++ website/static/index.js
@@ -16,39 +16,27 @@ function addPoints(noteId) {
   });
 }
 
-
+// task generator
 let quotes, quote, quoteauthor;
 let randNum;
 
 // Load JSON file into quotes
 fetch("quotes.txt")
-	.then((rawData) => rawData.text())
-	.then((data) => quotes = JSON.parse(data));
+  .then((response) => response.json())
+  .then((data) => { quotes = JSON.parse(data); });
+	// .then((rawData) => rawData.text())
+	// .then((data) => quotes = JSON.parse(data));
 
 function genQuote() {
+  console.log("wrking here too")
 	lastIndex = randNum;
 	randNum = Math.floor(Math.random() * quotes.length + 1); // Generate a Random Quote from the list
 	quote = quotes[randNum].inspire; // Gets the line of Quote
 	quoteauthor = quotes[randNum].author; // Gets the author of Quote
+  console.log("wrking here too")
 	document.getElementById("quote-line").innerHTML = '"' + quote + '"';
 	document.getElementById("quote-author").innerHTML = "- " + quoteauthor;
 }
 
+// weather widget
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-
-$(document).ready( function() {
-var i = 0;
-var txt = 'Welcome To Your Tasks';
-var speed = 60;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("maintitle").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-    document.getElementById("maintitle").classList.add("lets");
-  }
-}
- // This runs the displayTime function the first time
- typeWriter();
-});
